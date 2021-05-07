8bdff06d33302a375e2465fa2e9a292736d15fe2
AttilaDoro <doroattila@gmail.com>
2021-05-08 00:20:28 +0200

    update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
-const { getBuyActivitiesThatWereSoldLater } = require('../utils');
+const { getBuyActivitiesThatWereSoldLater, getAllActivityDates } = require('../utils');
 
 describe('getBuyActivitiesThatWereSoldLater', () => {
+
   it('Test #1', () => {
     const activities = [];
     const buyActivitiesThatWereSoldLater = getBuyActivitiesThatWereSoldLater(activities);
@@ -201,4 +202,142 @@ describe('getBuyActivitiesThatWereSoldLater', () => {
 
     assert.deepEqual(expected, buyActivitiesThatWereSoldLater);
   });
+
+});
+
+describe('getAllActivityDates', () => {
+
+  it('Test #1', () => {
+    const soldActivities = {};
+    const allActivityDates = getAllActivityDates(soldActivities);
+    const expected = [];
+    assert.deepEqual(expected, allActivityDates);
+  });
+
+  it('Test #2', () => {
+    const soldActivities = {
+      TSLA: {
+        buy: [
+          {
+            id: 1,
+            activityType: 'BUY',
+            symbol: 'TSLA',
+            tradeDate: '2021-05-20',
+          },
+        ],
+        sell: [
+          {
+            id: 2,
+            activityType: 'SELL',
+            symbol: 'TSLA',
+            tradeDate: '2021-10-19',
+          },
+        ],
+      },
+    };
+    const allActivityDates = getAllActivityDates(soldActivities);
+    const expected = ['20210520', '20211019'];
+    assert.deepEqual(expected, allActivityDates);
+  });
+
+  it('Test #3', () => {
+    const soldActivities = {
+      TSLA: {
+        buy: [
+          {
+            id: 1,
+            activityType: 'BUY',
+            symbol: 'TSLA',
+            tradeDate: '2021-05-20',
+          },
+        ],
+        sell: [
+          {
+            id: 2,
+            activityType: 'SELL',
+            symbol: 'TSLA',
+            tradeDate: '2021-10-19',
+          },
+        ],
+      },
+      SQ: {
+        buy: [
+          {
+            id: 3,
+            activityType: 'BUY',
+            symbol: 'TSLA',
+            tradeDate: '2021-03-12',
+          },
+        ],
+        sell: [
+          {
+            id: 4,
+            activityType: 'SELL',
+            symbol: 'TSLA',
+            tradeDate: '2021-11-11',
+          },
+        ],
+      },
+    };
+    const allActivityDates = getAllActivityDates(soldActivities);
+    const expected = ['20210312', '20210520', '20211019', '20211111'];
+    assert.deepEqual(expected, allActivityDates);
+  });
+
+  it('Test #3', () => {
+    const soldActivities = {
+      TSLA: {
+        buy: [
+          {
+            id: 1,
+            activityType: 'BUY',
+            symbol: 'TSLA',
+            tradeDate: '2021-05-20',
+          },
+          {
+            id: 2,
+            activityType: 'BUY',
+            symbol: 'TSLA',
+            tradeDate: '2021-06-20',
+          },
+        ],
+        sell: [
+          {
+            id: 3,
+            activityType: 'SELL',
+            symbol: 'TSLA',
+            tradeDate: '2021-10-19',
+          },
+        ],
+      },
+      SQ: {
+        buy: [
+          {
+            id: 4,
+            activityType: 'BUY',
+            symbol: 'TSLA',
+            tradeDate: '2021-03-12',
+          },
+          {
+            id: 5,
+            activityType: 'BUY',
+            symbol: 'TSLA',
+            tradeDate: '2021-05-20',
+          },
+        ],
+        sell: [
+          {
+            id: 6,
+            activityType: 'SELL',
+            symbol: 'TSLA',
+            tradeDate: '2021-11-11',
+          },
+        ],
+      },
+    };
+    const allActivityDates = getAllActivityDates(soldActivities);
+    const expected = ['20210312', '20210520', '20210620', '20211019', '20211111'];
+    assert.deepEqual(expected, allActivityDates);
+  });
+
 });
